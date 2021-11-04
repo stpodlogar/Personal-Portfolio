43242ff227361951fefe39cf0ca9b66ee35d2113
Stephen Podlogar <stpodlogar@gmail.com>
2021-11-04 13:47:37 -0400

    Changes: - adjust text alignment of about section responsively - change closeBtn according to light or dark mode

--- js/site.js
+++ js/site.js
@@ -1,11 +1,19 @@
 // Dark Mode Functionality
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const toggleIcon = document.querySelector('#toggle-icon');
+const closeBtn = document.querySelector('.btn-close');
+
+console.log(closeBtn);
 
 function toggleDarkLightMode(isDark) {
   toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
-  isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') :
+  if (isDark) {
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    closeBtn.classList.add('btn-close-white');
+  } else {
     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    closeBtn.classList.remove('btn-close-white');
+  } 
 }
 
 function switchTheme(event) {
